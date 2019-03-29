9e11695fae64d898a0badfeb2f06f0c0cea32ba0
Ferris <ferrisshang@gmail.com>
2019-03-29 17:55:55 +0800

    Add ftp_server

---
+++ ftp_server/run.py
@@ -0,0 +1,28 @@
+#! python2
+
+import socket
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+from pyftpdlib.authorizers import DummyAuthorizer
+
+def get_local_ip():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	try:
+		s.connect(('google.com', 80))
+		ip = s.getsockname()[0]
+		s.close()
+	except:
+		ip = 'N/A'
+	return ip
+
+def main():
+    authorizer = DummyAuthorizer()
+    authorizer.add_user('user', 'user', './../..', perm='elradfmwMT')
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    server = FTPServer(('', 21), handler)
+    server.serve_forever()
+
+if __name__ == "__main__":
+    print('IP: ' + get_local_ip())
+    main()
